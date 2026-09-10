58c5844287273c051b394042ff9f24d8315d1971
Brimi712 <britneyvalentin7@gmail.com>
2026-09-10 11:42:39 -0400

    Commiting gitignore to .venv

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# GLAB 385.2.1 - Lists and Methods
+# GLAB 385.2.1 - Lists and Methods Virtual Environment
+
+''' creating virtual environment name 
+
+'''
+# python -m venv <virtualEnvName .venv>
+# TO ACTIVATE VIRTUAL ENVIRONMENT - create an isolated environment for us to use
+
+# scripts: source .venv/Scripts/activate
